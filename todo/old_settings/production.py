0d1406791bfb0df78ac5664cbe32a9e1bbd738d1
Rovshen <rovshentagangylyjov@gmail.com>
2022-01-14 18:29:13 +0500

    testing: heroku deploy fails

--- todo/old_settings/production.py
+++ todo/old_settings/production.py
@@ -9,19 +9,6 @@ ALLOWED_HOSTS = ["todo-django4.herokuapp.com"]
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# Security
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = 15768000  # set low, but when site is ready for deployment, set to at least 15768000 (6 months)
-SECURE_SSL_REDIRECT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-CSRF_COOKIE_SAMESITE = 'Strict'
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-X_FRAME_OPTIONS = 'DENY'
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
